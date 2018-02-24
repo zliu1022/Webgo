6fe1426335eb8dd92f7fb5b6c248531f967de48a
zliu1022 <zliu1022@gmail.com>
2018-02-24 13:42:15 +0800

    重新合入了sabaki的主题
    原打算采用sabaki的图形棋子，但使用了Board.drawHandlers.REALISTIC没有搞定，暂时放弃，sabaki的主题目录整理了暂时放着

--- board/themes/1bb97ba0-d8cf-11e7-aec5-5d31ed3edf58/renderer/api/module-list.js
+++
@@ -1,8 +0,0 @@
-// Renderer side modules, please sort alphabetically.
-module.exports = [
-  {name: 'desktopCapturer', file: 'desktop-capturer'},
-  {name: 'ipcRenderer', file: 'ipc-renderer'},
-  {name: 'remote', file: 'remote'},
-  {name: 'screen', file: 'screen'},
-  {name: 'webFrame', file: 'web-frame'}
-]
