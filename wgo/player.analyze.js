8dcee4e645a68df39bde9e7d56220b32237dd272
zliu1022 <zliu1022@gmail.com>
2019-02-02 15:18:42 +0800

    Update player.analyze.js

--- wgo/player.analyze.js
+++ wgo/player.analyze.js
@@ -730,7 +730,7 @@ ws.onmessage = function (evt) {
                 }
             }
 
-            if (ret.result[0].visits>1){
+            if (ret.result[0].visits>100){
                 WGo.Player.Analyze.manual_play(ret.result[0].x, ret.result[0].y)
             }
 
